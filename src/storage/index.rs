614d2f9da09932125d04d5e8ef1549e6015f6491
aabb15768 <b03204032@ntu.edu.tw>
2018-12-23 22:03:36 +0800

    add string version 2.0

--- src/storage/index.rs
+++ src/storage/index.rs
@@ -17,6 +17,10 @@ struct IndexDataStructureInt {
     row: u32,
     key_value: u32,
 }
+struct IndexDataStructureString {
+    row: u32,
+    key_value: Vec<u8>
+}
 
 
 
@@ -25,7 +29,7 @@ fn type_int_construct(table_info: TableInfo) {
     let mut bytes_to_slide = table_info.row_bytes - table_info.key_bytes;
     let mut index_arr: Vec<IndexDataStructureInt> = Vec::new();
     let table_name = table_info.table_name.clone();
-    let tableIndexName = table_name.clone() + "index";
+    let table_index_name = table_name.clone() + "index";
     let mut file = File::open(table_name).unwrap();
     file.seek(SeekFrom::Start(table_info.key_offet as u64));
     let mut buffer = [0;4];
@@ -51,14 +55,50 @@ fn type_int_construct(table_info: TableInfo) {
     }
 
     index_arr.sort_unstable_by(|a, b| a.key_value.cmp(&b.key_value));
-    let mut fileWrite = File::create(tableIndexName).unwrap();
+    let mut file_write = File::create(table_index_name).unwrap();
     for i in 0..index_arr.len() {
-        write!(fileWrite, "{:b}{:b}", index_arr[i].row, index_arr[i].key_value);
+        let row_temp = unsafe{mem::transmute::<u32, [u8; 4]>(index_arr[i].row)};
+        file_write.write(&row_temp);
+        let key_temp = unsafe{mem::transmute::<u32, [u8; 4]>(index_arr[i].key_value)};
+        file_write.write(&key_temp);
     }
+
 }
 
 fn type_string_construct(table_info: TableInfo) {
-    //TODO....
+    let mut row = 0;
+    let mut bytes_to_slide = table_info.row_bytes - table_info.key_bytes;
+    let mut index_arr: Vec<IndexDataStructureString> = Vec::new();
+    let table_name = table_info.table_name.clone();
+    let table_index_name = table_name.clone() + "index";
+    let mut file = File::open(table_name).unwrap();
+    file.seek(SeekFrom::Start(table_info.key_offet as u64));
+    let mut buffer = vec![0; table_info.key_bytes as usize];
+    loop {
+        let bytes_read = match file.read(&mut buffer) {
+            Ok(0) => break, // end-of-file
+            Ok(n) => {
+                let mut index_content = IndexDataStructureString {
+                    row: row,
+                    key_value: buffer.clone(),
+                };
+                index_arr.push(index_content);
+                file.seek(SeekFrom::Current(bytes_to_slide as i64));
+                row = row + 1;
+            },
+            Err(e) => {
+                println!("error{}", e);
+            }
+        };
+    }
+
+    index_arr.sort_unstable_by(|a, b| a.key_value.cmp(&b.key_value));
+    let mut file_write = File::create(table_index_name).unwrap();
+    for i in 0..index_arr.len() {
+        let row_temp = unsafe{mem::transmute::<u32, [u8; 4]>(index_arr[i].row)};
+        file_write.write(&row_temp);
+        file_write.write(&index_arr[i].key_value);
+    }
 }
 
 pub fn construct_index(table_info: TableInfo) {
