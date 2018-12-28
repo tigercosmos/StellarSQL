8cfbd0f980c9a8b7d1d52f665ac6e17c407ef826
aabb15768 <b03204032@ntu.edu.tw>
2018-12-28 21:31:15 +0800

    add error message and return value version 4.0

--- src/storage/index.rs
+++ src/storage/index.rs
@@ -1,12 +1,32 @@
-use std::fs::File;
 use std::io;
-use std::io::prelude::*;
-use std::io::Seek;
+use std::fs::File;
 use std::io::SeekFrom;
+use std::io::Seek;
+use std::io::prelude::*;
+use std::fmt;
 use std::mem;
 
+#[derive(Debug)]
+pub enum IndexErr {
+    BuildIntIndexTableError,
+    ReadIntIndexTableError,
+    BuildStringIndexTableError,
+    ReadStringIndexTableError,
+}
+
+impl fmt::Display for IndexErr {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        match *self {
+            IndexErr::BuildIntIndexTableError => write!(f, "Build int index table error"),
+            IndexErr::ReadIntIndexTableError => write!(f, "Read int index table error"),
+            IndexErr::BuildStringIndexTableError => write!(f, "Build string index table error"),
+            IndexErr::ReadStringIndexTableError => write!(f, "Read string index table error"),
+        }
+    }
+}
+
 // meta data of raw table
-struct TableMeta {
+pub struct TableMeta {
     table_name: String, // name of raw table
     key_type: String,   // type of primary key in raw table
     key_offet: u32,     // byte position of first primary key in raw table
@@ -15,18 +35,22 @@ struct TableMeta {
 }
 
 // row and key value pair in which key type is int
-struct IndexDataStructureInt {
+pub struct IndexDataStructureInt {
     row: u32,
     key_value: u32,
 }
 // row and key value pair in which key type is string
-struct IndexDataStructureString {
+pub struct IndexDataStructureString {
     row: u32,
-    key_value: Vec<u8>,
+    key_value: Vec<u8>
 }
 
+pub struct IndexInt(Vec<IndexDataStructureInt>);
+pub struct IndexString(Vec<IndexDataStructureString>);
+
+
 // build index table with raw table in whicj key type is int
-fn build_int_index_table(table_meta: &TableMeta, index_arr: &mut Vec<IndexDataStructureInt>) {
+pub fn build_int_index_table(table_meta: &TableMeta, index_arr: &mut IndexInt) -> Result<(), IndexErr> {
     let mut row = 0;
     let mut bytes_to_slide = table_meta.row_bytes - table_meta.key_bytes;
     let table_name = table_meta.table_name.clone();
@@ -43,34 +67,36 @@ fn build_int_index_table(table_meta: &TableMeta, index_arr: &mut Vec<IndexDataSt
                         row: row,
                         key_value: temp,
                     };
-                    index_arr.push(index_content);
+                    index_arr.0.push(index_content);
                 }
                 file.seek(SeekFrom::Current(bytes_to_slide as i64));
                 row = row + 1;
-            }
+            },
             Err(e) => {
-                println!("build_int_index_table error{}", e);
+                return Err(IndexErr::BuildIntIndexTableError);
             }
         };
     }
 
-    index_arr.sort_unstable_by(|a, b| a.key_value.cmp(&b.key_value));
+    index_arr.0.sort_unstable_by(|a, b| a.key_value.cmp(&b.key_value));
+    Ok(())
 }
 
 // write index table into index file
-fn write_int_index_table(table_meta: &TableMeta, index_arr: &mut Vec<IndexDataStructureInt>) {
+pub fn write_int_index_table(table_meta: &TableMeta, index_arr: &mut IndexInt) -> Result<(), IndexErr> {
     let table_index_name = table_meta.table_name.clone() + "index";
     let mut file_write = File::create(table_index_name).unwrap();
-    for i in 0..index_arr.len() {
-        let row_temp = unsafe { mem::transmute::<u32, [u8; 4]>(index_arr[i].row) };
+    for i in 0..index_arr.0.len() {
+        let row_temp = unsafe{mem::transmute::<u32, [u8; 4]>(index_arr.0[i].row)};
         file_write.write(&row_temp);
-        let key_temp = unsafe { mem::transmute::<u32, [u8; 4]>(index_arr[i].key_value) };
+        let key_temp = unsafe{mem::transmute::<u32, [u8; 4]>(index_arr.0[i].key_value)};
         file_write.write(&key_temp);
     }
+    Ok(())
 }
 
 // read index table from index file
-fn read_int_index_table(table_meta: &TableMeta, index_arr: &mut Vec<IndexDataStructureInt>) {
+pub fn read_int_index_table(table_meta: &TableMeta, index_arr: &mut IndexInt) -> Result<(), IndexErr> {
     let table_index_name = table_meta.table_name.clone() + "index";
     let mut file = File::open(table_index_name).unwrap();
     let mut buffer_row = [0; 4];
@@ -78,48 +104,51 @@ fn read_int_index_table(table_meta: &TableMeta, index_arr: &mut Vec<IndexDataStr
     loop {
         let bytes_read = match file.read(&mut buffer_row) {
             Ok(0) => break, // end-of-file
-            Ok(n) => unsafe {
-                let temp_row = mem::transmute::<[u8; 4], u32>(buffer_row);
-                file.read(&mut buffer_key);
-                let temp_key = mem::transmute::<[u8; 4], u32>(buffer_key);
-                let mut index_content = IndexDataStructureInt {
-                    row: temp_row,
-                    key_value: temp_key,
-                };
-                index_arr.push(index_content);
+            Ok(n) => {
+                unsafe {
+                    let temp_row = mem::transmute::<[u8; 4], u32>(buffer_row);
+                    file.read(&mut buffer_key);
+                    let temp_key = mem::transmute::<[u8; 4], u32>(buffer_key);
+                    let mut index_content = IndexDataStructureInt {
+                        row: temp_row,
+                        key_value: temp_key,
+                    };
+                    index_arr.0.push(index_content);
+                }
             },
             Err(e) => {
-                println!("read_int_index_table error{}", e);
+                return Err(IndexErr::ReadIntIndexTableError);
             }
         };
     }
+    Ok(())
 }
 
 // insert into index table in which key type is int
 // if work, use b-insert
-fn insert_int_index_table(insert_value: IndexDataStructureInt, index_arr: &mut Vec<IndexDataStructureInt>) {
-    if (index_arr.is_empty()) {
-        index_arr.push(insert_value);
+pub fn insert_int_index_table(insert_value: IndexDataStructureInt, index_arr: &mut IndexInt) -> Result<(), IndexErr> {
+    if(index_arr.0.is_empty()) {
+        index_arr.0.push(insert_value);
     } else {
         let mut target = 0;
-        for i in 0..index_arr.len() {
-            if (insert_value.key_value <= index_arr[i].key_value) {
+        for i in 0..index_arr.0.len() {
+            if(insert_value.key_value <= index_arr.0[i].key_value) {
                 target = i;
                 break;
             }
         }
-        if (target == 0) {
-            index_arr.insert(target, insert_value);
+        if(target == 0) {
+            index_arr.0.insert(target, insert_value);
         } else {
-            index_arr.insert(target - 1, insert_value);
+            index_arr.0.insert(target-1, insert_value);
         }
     }
+    Ok(())
 }
 
-fn build_string_index_table(table_meta: &TableMeta, index_arr: &mut Vec<IndexDataStructureString>) {
+pub fn build_string_index_table(table_meta: &TableMeta, index_arr: &mut IndexString) -> Result<(), IndexErr> {
     let mut row = 0;
     let mut bytes_to_slide = table_meta.row_bytes - table_meta.key_bytes;
-    let mut index_arr: Vec<IndexDataStructureString> = Vec::new();
     let table_name = table_meta.table_name.clone();
     let mut file = File::open(table_name).unwrap();
     file.seek(SeekFrom::Start(table_meta.key_offet as u64));
@@ -132,30 +161,32 @@ fn build_string_index_table(table_meta: &TableMeta, index_arr: &mut Vec<IndexDat
                     row: row,
                     key_value: buffer.clone(),
                 };
-                index_arr.push(index_content);
+                index_arr.0.push(index_content);
                 file.seek(SeekFrom::Current(bytes_to_slide as i64));
                 row = row + 1;
-            }
+            },
             Err(e) => {
-                println!("error{}", e);
+                return Err(IndexErr::BuildStringIndexTableError);
             }
         };
     }
 
-    index_arr.sort_unstable_by(|a, b| a.key_value.cmp(&b.key_value));
+    index_arr.0.sort_unstable_by(|a, b| a.key_value.cmp(&b.key_value));
+    Ok(())
 }
 
-fn write_string_index_table(table_meta: &TableMeta, index_arr: &mut Vec<IndexDataStructureString>) {
+pub fn write_string_index_table(table_meta: &TableMeta, index_arr: &mut IndexString) -> Result<(), IndexErr> {
     let table_index_name = table_meta.table_name.clone() + "index";
     let mut file_write = File::create(table_index_name).unwrap();
-    for i in 0..index_arr.len() {
-        let row_temp = unsafe { mem::transmute::<u32, [u8; 4]>(index_arr[i].row) };
+    for i in 0..index_arr.0.len() {
+        let row_temp = unsafe{mem::transmute::<u32, [u8; 4]>(index_arr.0[i].row)};
         file_write.write(&row_temp);
-        file_write.write(&index_arr[i].key_value);
+        file_write.write(&index_arr.0[i].key_value);
     }
+    Ok(())
 }
 
-fn read_string_index_table(table_meta: &TableMeta, index_arr: &mut Vec<IndexDataStructureString>) {
+pub fn read_string_index_table(table_meta: &TableMeta, index_arr: &mut IndexString) -> Result<(), IndexErr> {
     let table_index_name = table_meta.table_name.clone() + "index";
     let mut file = File::open(table_index_name).unwrap();
     let mut buffer_row = [0; 4];
@@ -163,43 +194,47 @@ fn read_string_index_table(table_meta: &TableMeta, index_arr: &mut Vec<IndexData
     loop {
         let bytes_read = match file.read(&mut buffer_row) {
             Ok(0) => break, // end-of-file
-            Ok(n) => unsafe {
-                let temp_row = mem::transmute::<[u8; 4], u32>(buffer_row);
-                file.read(&mut buffer_key);
-                let mut index_content = IndexDataStructureString {
-                    row: temp_row,
-                    key_value: buffer_key.clone(),
-                };
-                index_arr.push(index_content);
+            Ok(n) => {
+                unsafe {
+                    let temp_row = mem::transmute::<[u8; 4], u32>(buffer_row);
+                    file.read(&mut buffer_key);
+                    let mut index_content = IndexDataStructureString {
+                        row: temp_row,
+                        key_value: buffer_key.clone(),
+                    };
+                    index_arr.0.push(index_content);
+                }
             },
             Err(e) => {
-                println!("read_int_index_table error{}", e);
+                return Err(IndexErr::ReadStringIndexTableError);
             }
         };
     }
+    Ok(())
 }
 
-fn insert_string_index_table(insert_value: IndexDataStructureString, index_arr: &mut Vec<IndexDataStructureString>) {
-    if (index_arr.is_empty()) {
-        index_arr.push(insert_value);
+pub fn insert_string_index_table(insert_value: IndexDataStructureString, index_arr: &mut IndexString) -> Result<(), IndexErr> {
+    if(index_arr.0.is_empty()) {
+        index_arr.0.push(insert_value);
     } else {
         let mut target = 0;
-        for i in 0..index_arr.len() {
-            if (insert_value.key_value < index_arr[i].key_value) {
+        for i in 0..index_arr.0.len() {
+            if(insert_value.key_value <= index_arr.0[i].key_value) {
                 target = i;
                 break;
             }
         }
-        if (target == 0) {
-            index_arr.insert(target, insert_value);
+        if(target == 0) {
+            index_arr.0.insert(target, insert_value);
         } else {
-            index_arr.insert(target - 1, insert_value);
+            index_arr.0.insert(target-1, insert_value);
         }
     }
+    Ok(())
 }
 
-// test
-fn test_construct_index() {
+#[test]
+pub fn test_construct_index() {
     let table_meta = TableMeta {
         table_name: String::from("1.in"),
         key_type: String::from("Int"),
@@ -207,12 +242,13 @@ fn test_construct_index() {
         key_bytes: 4,
         row_bytes: 4,
     };
-    let mut index_arr: Vec<IndexDataStructureInt> = Vec::new();
+    let mut index_arr = IndexInt(vec![]);
     build_int_index_table(&table_meta, &mut index_arr);
     write_int_index_table(&table_meta, &mut index_arr);
 }
 
-fn test_read_index() {
+#[test]
+pub fn test_read_index() {
     let table_meta = TableMeta {
         table_name: String::from("1.in"),
         key_type: String::from("Int"),
@@ -220,7 +256,7 @@ fn test_read_index() {
         key_bytes: 4,
         row_bytes: 4,
     };
-    let mut index_arr: Vec<IndexDataStructureInt> = Vec::new();
+    let mut index_arr = IndexInt(vec![]);
     read_int_index_table(&table_meta, &mut index_arr);
     write_int_index_table(&table_meta, &mut index_arr);
 }
